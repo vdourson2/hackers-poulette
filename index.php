ff7c2c8f0cc581848faa3de658514c6d685c0400
Virginie Dourson <virginiedourson@gmail.com>
2023-11-06 17:53:44 +0100

    Sanitize and validate email

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php  
+
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+
+$error_email = null;
+
+
+if (isset($_POST['submit'])) {
+    //If the email is completed
+    if (isset($_POST['email'])){
+        $email=filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+        echo filter_var($email,FILTER_VALIDATE_EMAIL);
+        //The sanitized email is valid
+        if (filter_var($email,FILTER_VALIDATE_EMAIL)){
+            header('location:php/checkout.php');
+        }
+        //The sanitized email is not valid 
+        else {
+            $error_email = 'Your email is not valid'; 
+        }
+    //The email is not completed
+    } else {
+        $error_email = 'You must complete your email';
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,20 +42,20 @@
         <h1>Hackers Poulette</h1>
     </header>
     <h2>Contact support</h2>
-    <form class="form" id="form" name="form" autocomplete="off" action="php/index.php" method="post">
+    <form class="form" id="form" name="form" autocomplete="off" action="" method="post">
         <fieldset class="personal">
             <legend class="personal__legend">Personal information</legend>
             <div class="personal__wrapName">
                 <label for="name">Name : </label>
-                <input type="text" id="name" name="name" required/>
+                <input type="text" id="name" name="name" />
             </div>
             <div class="personal__wrapLastName">
                 <label for="lastname">Lastname : </label>
-                <input type="text" id="lastname" name="lastname" required/>
+                <input type="text" id="lastname" name="lastname" />
             </div>
             <div class="personal__wrapGender">
                 <label for="gender">Gender : </label>
-                <select id="gender" name="gender" required>
+                <select id="gender" name="gender" >
                     <option value="" selected>Gender</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
@@ -33,12 +63,13 @@
                 </select>
             </div>
             <div class="personal__wrapEmail">
+                <p><?php echo $error_email; ?></p>
                 <label for="email">Email : </label>
-                <input type="email" id="email" name="email" required/>
+                <input type="email" id="email" name="email" />
             </div>
             <div class="personal__wrapCountry">
                 <label for="country">Country : </label>
-                <select id="country" name="country" required>
+                <select id="country" name="country" >
                     <option value="" selected>Country</option>
                     <option value="belgium">Belgium</option>
                     <option value="france">France</option>
@@ -60,10 +91,10 @@
             </div> 
             <div class="question__wrapMessage">
                 <label for="message">Your message : </label>
-                <textarea name="message" id="message" cols="30" rows="10" required></textarea>
+                <textarea name="message" id="message" cols="30" rows="10"></textarea>
             </div>
         </fieldset>
-        <Button class="button" type="submit">Submit</Button>
+        <Button class="button" type="submit" name="submit" value="submit">Submit</Button>
     </form>
 
 </body>
---
+++ php/checkout.php
@@ -0,0 +1,2 @@
+<?php 
+echo "That's it";
